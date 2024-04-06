0861e970f9cd32fc383d2750744d68a785d2ba73
Vincent <vincent.arnal13@gmail.com>
2024-04-06 16:38:12 -0400

    adding steps

--- src/app/components/plateau/plateau.component.ts
+++ src/app/components/plateau/plateau.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { MatDrawer, MatDrawerContainer } from "@angular/material/sidenav";
+import { MatButton, MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { NgIf, NgTemplateOutlet } from "@angular/common";
+import { MatCheckbox } from "@angular/material/checkbox";
+import { StepComponent } from "../step/step.component";
 
 @Component({
-  selector: 'oie-plateau',
+  selector: "oie-plateau",
   standalone: true,
-  imports: [],
-  templateUrl: './plateau.component.html',
-  styleUrl: './plateau.component.scss'
+  imports: [
+    MatDrawerContainer,
+    MatDrawer,
+    MatButton,
+    MatIconButton,
+    MatIcon,
+    NgTemplateOutlet,
+    MatCheckbox,
+    NgIf,
+    StepComponent
+  ],
+  templateUrl: "./plateau.component.html",
+  styleUrl: "./plateau.component.scss"
 })
 export class PlateauComponent {
+  showFiller = false;
 
+  @Input() withSteps = false;
+
+  protected readonly PlateauType = PlateauType;
+}
+
+export enum PlateauType {
+  WITH = "with",
+  WITHOUT = "without"
 }
---
+++ src/app/components/step/step.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'oie-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './step.component.html',
+  styleUrl: './step.component.scss'
+})
+export class StepComponent {
+
+  @Input() value= 'ein';
+  @Input() type = StepType.BUBBLE;
+
+}
+
+
+export enum StepType {
+  STAR = "star",
+  BUBBLE = "bubble",
+  START_END = "start_end"
+}
